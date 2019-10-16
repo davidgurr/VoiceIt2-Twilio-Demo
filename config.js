055bad04bf07107304ecd51a3da4c54fbb9d934f
davidgurr <34814601+davidgurr@users.noreply.github.com>
2019-10-16 11:16:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 const config = {
   apiKey : process.env.API_KEY,
   apiToken : process.env.API_TOKEN,
-  contentLanguage: process.env.CONTENT_LANGUAGE,
-  chosenVoicePrintPhrase: process.env.VOICEPRINT_PHRASE,
-  dataBaseURL:  process.env.DATABASE_URL
+  airTableKey: process.env.AIRTABLE_KEY,
+  airTableBase: process.env.AIRTABLE_BASE
 }
 
 module.exports = config;
