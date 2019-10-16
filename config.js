96c4ea963a8d400e7cd03e99b5d735e0e15590f0
davidgurr <34814601+davidgurr@users.noreply.github.com>
2019-10-16 11:22:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const config = {
   apiKey : process.env.API_KEY,
   apiToken : process.env.API_TOKEN,
   airTableKey: process.env.AIRTABLE_KEY,
-  airTableBase: process.env.AIRTABLE_BASE
+  airTableBase: process.env.AIRTABLE_BASE,
+  twilioFlow: process.env.TWILIO_FLOW
 }
 
 module.exports = config;
